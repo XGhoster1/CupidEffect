8b0e26f65e7b273e5573cd154b4e6323572987c8
TegogolO <odiegoalejandro@gmail.com>
2024-10-23 11:41:20 +0200

    Added a matching function

--- Matching_algorithm.py
+++ Matching_algorithm.py
@@ -1,3 +1,9 @@
 print("hello Cupid is here!")
 
 print("hola mamasita")
+
+
+a = input("Enter participant a")
+b = input("Enter participant b")
+
+print("Participant a has been matched with participant b ! Congratulations !")
